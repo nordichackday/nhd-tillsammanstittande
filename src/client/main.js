9ac19613298d0e8971b59a31548a860368b51825
Alexander Ekman <alexander.ekman@svt.se>
2016-04-28 18:20:06 +0200

    Added placeholder start page and some easy javascript

--- src/client/main.js
+++ src/client/main.js
@@ -1,11 +1,24 @@
 var socket = io('http://localhost:4000');
 
+var $users = $('#users');
+
 socket.on('list', function (data) {
 	console.log('User list: ', data);
-
-	var $users = $('#users');
 
 	$(data.users).each(function (index) {
 		$users.append('<li><b>' + this.name + '</b><br>Tittar på: ' + this.watching.title + 	'</li>');
 	});
+});
+
+
+$('.placeholder').click(function () {
+	// $(this).css({
+	// 	left: -200
+	// });
+
+	// $users.css({
+	// 	right: 0
+	// });
+
+	$('body').addClass('menu-open');
 });
