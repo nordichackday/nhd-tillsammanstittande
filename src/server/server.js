e48ab917d8d4fb9250f3e62564fe407f3aa2bede
cristobal <cristobal@dabed.org>
2016-04-29 10:51:40 +0200

    fix typo

--- src/server/server.js
+++ src/server/server.js
@@ -5,7 +5,7 @@ import webpackMiddleware from 'server/middleware/webpack';
 import distMiddleware    from 'server/middleware/dist';
 
 const isDeveloping = process.env.NODE_ENV !== 'production';
-const port         = process.env.PORT : 3000;
+const port         = process.env.PORT || 3000;
 
 const app = express();
 
