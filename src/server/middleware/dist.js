682b611442eb12d5221df98d94be110419eac2d2
Alexander Ekman <alexander.ekman@svt.se>
2016-04-29 07:55:24 +0200

    Test

--- src/server/middleware/dist.js
+++ src/server/middleware/dist.js
@@ -3,8 +3,8 @@ import path    from 'path';
 
 export default function (app) {
 
-  app.use(express.static(__dirname + '/dist'));
+  app.use(express.static(__dirname + '/../../public'));
   app.get('*', function response(req, res) {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
   });
 }
